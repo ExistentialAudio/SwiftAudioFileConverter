3b02b0fbd345bf0b6dd2165a9e7540ebf341a5b0
Devin Roth <devin@devinrothmusic.com>
2025-04-03 21:48:24 -0700

    Remove fileExists().

--- Sources/SwiftAudioFileConverter/SwiftAudioFileConverter/SwiftAudioFileConverter.swift
+++ Sources/SwiftAudioFileConverter/SwiftAudioFileConverter/SwiftAudioFileConverter.swift
@@ -6,22 +6,17 @@ public actor SwiftAudioFileConverter {
                         to outputURL: URL,
                         with settings: AudioFileSettings) async throws {
         
-        // 1. Check if the input file extension is a known audio format
+        // Check if the input file extension is a known audio format
         guard FileFormat.allCases.map(\.rawValue).contains(inputURL.pathExtension.lowercased()) else {
             throw SwiftAudioFileConverterError.unsupportedAudioFileExtension(inputURL)
         }
         
-        // 2. If the file exists
-        guard FileManager.default.fileExists(atPath: inputURL.path()) else {
-            throw SwiftAudioFileConverterError.fileDoesNotExist(inputURL)
-        }
-        
-        // 3. Check read permissions on the input file
+        // Check read permissions on the input file
         guard FileManager.default.isReadableFile(atPath: inputURL.path) else {
             throw SwiftAudioFileConverterError.fileIsNotReadable(inputURL)
         }
         
-        // 4. Check write permissions on the output (directory must be writable)
+        // Check write permissions on the output (directory must be writable)
         //    NOTE: For a brand new file, isWritableFile(atPath:) can be misleading.
         //    Often, you'll want to check if the directory is writable instead.
         let outputDirectory = outputURL.deletingLastPathComponent()
@@ -29,12 +24,12 @@ public actor SwiftAudioFileConverter {
             throw SwiftAudioFileConverterError.fileIsNotWritable(outputURL)
         }
         
-        // 5. Check if output extension matches settings.fileFormat
+        // Check if output extension matches settings.fileFormat
         guard outputURL.pathExtension.lowercased() == settings.fileFormat.rawValue else {
             throw SwiftAudioFileConverterError.audioFileExtensionSettingsMismatch(outputURL, settings)
         }
         
-        // 6. Dispatch the conversion based on the requested format
+        // Dispatch the conversion based on the requested format
         switch settings.fileFormat {
             
         case .wav, .aiff, .aac, .alac:
