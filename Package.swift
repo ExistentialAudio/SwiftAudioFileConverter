52786e97f40d70a5d199d8ec63a3fec5c50dc74d
Steffan Andrews <stef@orchetect.com>
2025-11-08 17:23:30 -0800

    Package.swift cleanup

--- Package.swift
+++ Package.swift
@@ -1,24 +1,21 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftAudioFileConverter",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v13), .iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftAudioFileConverter",
-            targets: ["SwiftAudioFileConverter"]),
+            targets: ["SwiftAudioFileConverter"]
+        )
     ],
     dependencies: [
-    .package(url: "https://github.com/Phisto/swift-lame.git", .upToNextMajor(from: "3.100.0")),
-    .package(url: "https://github.com/sbooth/CFLAC.git", from: "1.3.2")
+        .package(url: "https://github.com/Phisto/swift-lame.git", .upToNextMajor(from: "3.100.0")),
+        .package(url: "https://github.com/sbooth/CFLAC.git", from: "1.3.2")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftAudioFileConverter",
             dependencies: [
@@ -31,6 +28,6 @@ let package = Package(
             name: "SwiftAudioFileConverterTests",
             dependencies: ["SwiftAudioFileConverter"],
             swiftSettings: [.interoperabilityMode(.Cxx)]
-        ),
+        )
     ]
 )
