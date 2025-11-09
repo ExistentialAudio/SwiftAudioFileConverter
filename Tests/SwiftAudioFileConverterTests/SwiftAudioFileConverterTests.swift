dfe1f137b9d5632e18cade948a74552f1785d2ce
Steffan Andrews <stef@orchetect.com>
2025-11-08 17:41:12 -0800

    Cleaned up test target

--- Tests/SwiftAudioFileConverterTests/SwiftAudioFileConverterTests.swift
+++ Tests/SwiftAudioFileConverterTests/SwiftAudioFileConverterTests.swift
@@ -2,20 +2,21 @@ import Foundation
 import Testing
 @testable import SwiftAudioFileConverter
 
+/// Not a unit test. A simple test harness to perform a local file conversion.
 @available(macOS 13.0, *)
 @available(iOS, unavailable)
 @available(tvOS, unavailable)
 @available(watchOS, unavailable)
 @available(visionOS, unavailable)
-@Test func example() async throws {
+@Test func manualConversion() async throws {
     let settings = try AudioFileSettings(
         sampleRate: .kHz48,
         bitDepth: .float32,
         fileFormat: .flac,
         channelFormat: .stereo)
     
-    let inputURL = URL.desktopDirectory.appending(path: "dipper.wav")
-    let outputURL = URL.desktopDirectory.appending(path: "dipper.flac")
+    let inputURL = URL.desktopDirectory.appending(path: "test.wav")
+    let outputURL = URL.desktopDirectory.appending(path: "test.flac")
     
     try await SwiftAudioFileConverter.convert(
         from: inputURL,
