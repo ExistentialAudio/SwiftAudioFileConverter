090886f244109ae485576d85d1fb1fcd89a90be4
Steffan Andrews <stef@orchetect.com>
2025-11-08 17:24:27 -0800

    Error types now conform to LocalizedError with English error descriptions.

--- Sources/SwiftAudioFileConverter/Errors/SwiftAudioFileConverterError.swift
+++ Sources/SwiftAudioFileConverter/Errors/SwiftAudioFileConverterError.swift
@@ -4,9 +4,10 @@
 //
 //  Created by Devin Roth on 2025-04-03.
 //
+
 import Foundation
 
-public enum SwiftAudioFileConverterError: Error {
+public enum SwiftAudioFileConverterError: LocalizedError {
     case invalidAudioFileSettings(AudioFileSettings)
     case unsupportedAudioFileExtension(URL)
     case audioFileExtensionSettingsMismatch(URL, AudioFileSettings)
@@ -17,4 +18,37 @@ public enum SwiftAudioFileConverterError: Error {
     case unsupportedConversion(FileFormat)
     case coreAudioError(CoreAudioError)
     case flacConversionUnknownError
+}
+
+extension SwiftAudioFileConverterError: Equatable { }
+
+extension SwiftAudioFileConverterError: Hashable { }
+
+extension SwiftAudioFileConverterError: Sendable { }
+
+extension SwiftAudioFileConverterError {
+    public var errorDescription: String? {
+        switch self {
+        case let .invalidAudioFileSettings(audioFileSettings):
+            "Invalid audio file settings. \(audioFileSettings)"
+        case let .unsupportedAudioFileExtension(url):
+            "Unsupported audio file extension (\(url.path))"
+        case let .audioFileExtensionSettingsMismatch(url, audioFileSettings):
+            "Audio file extension settings mismatch. (\(url.path)) \(audioFileSettings)"
+        case let .fileDoesNotExist(url):
+            "File does not exist. (\(url.path))"
+        case let .fileIsNotReadable(url):
+            "File is not readable. (\(url.path))"
+        case let .unableToOpenFile(url):
+            "Unable to open file. (\(url.path))"
+        case let .fileIsNotWritable(url):
+            "File is not writable. (\(url.path))"
+        case let .unsupportedConversion(fileFormat):
+            "Unsupported conversion: \(fileFormat)"
+        case let .coreAudioError(coreAudioError):
+            "Core Audio error: \(coreAudioError)"
+        case .flacConversionUnknownError:
+            "Unknown FLAC conversion error."
+        }
+    }
 }
